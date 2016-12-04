60663424eddcbe8eb8b2223cb9943b1b72797648
nicmarcelo <nfmarcelos@gmail.com>
2016-12-04 18:27:15 -0500

    Test Cliente

---
+++ CFFLORES.TestRest/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace CFFLORES.TestRest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                string busqueda = "2";
+                string valor = "12345678";
+                //dgvVenta.DataSource = daoproducto.ListarProducto();
+                string URLAuth = "http://localhost:24832/Cliente.svc/Clientes?Gbusqueda=" + busqueda + "&Gvalor=" + valor.ToString();
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.
+                    Create(URLAuth);
+                req.Method = "GET";
+                req.ContentType = "application/json";
+                var res = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(res.GetResponseStream());
+                string clienteJson = reader.ReadToEnd();
+                JavaScriptSerializer JsonConvert = new JavaScriptSerializer();
+ 
+
+
+            }
+            catch (WebException ex)
+            {
+                HttpStatusCode code = ((HttpWebResponse)ex.Response).StatusCode;
+                string message = ((HttpWebResponse)ex.Response).StatusDescription;
+                StreamReader reader = new StreamReader(ex.Response.GetResponseStream());
+                string error = reader.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string mensaje = js.Deserialize<string>(error);
+
+               
+
+            }
+        }
+    }
+}
