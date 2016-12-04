77c64cea35578a06ed966481d0e6393aa7528b27
nicmarcelo <nfmarcelos@gmail.com>
2016-12-04 18:20:39 -0500

    Dominio Cliente

---
+++ CFFLORES.WebService/Dominio/ECliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CFFLORES.WebService.Dominio
+{
+    [DataContract]
+    public class ECliente
+    {
+        [DataMember]
+        public int IdCliente { get; set; }
+        [DataMember]
+        public string Dni { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public string Direccion { get; set; }
+
+
+
+        
+    }
+}
